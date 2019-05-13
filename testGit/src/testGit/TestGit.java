156d2b1800444d4111190a9ef87b76d5b4230429
zhangchuhao <50584930+zhangchuhao@users.noreply.github.com>
2019-05-13 22:27:46 +0800

    Update TestGit.java

--- testGit/src/testGit/TestGit.java
+++ testGit/src/testGit/TestGit.java
@@ -2,10 +2,12 @@ package testGit;
 
 public class TestGit {
 	
-	int b =2;
+	int b =3;
+	int a =3;
 	public static void main(String[] args) {
-		System.out.println("测试第二次");
-
+		
+		System.out.println("测试第二  次");
+   
 	}
 public void say(){
  System.out.println("测试第二次");
